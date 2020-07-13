51778feb984e7685c8b32a5596521306e2ef19ac
gilmujjang <alsanrlf@naver.com>
2020-07-13 16:35:08 +0900

    노마드코더예제 그대로따라침

---
+++ javascript/todolist/todo.js
@@ -0,0 +1,69 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+const TODOS_LS = 'toDos';
+
+let toDos = [];
+
+function filterFn(toDo){
+    return toDo.id === 1
+}
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !==parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteToDo);
+    const span = document.createElement("span");
+    const newId = toDos.length +1;
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos(){
+    const loadedtoDos = localStorage.getItem(TODOS_LS);
+    if(loadedtoDos !== null){
+        const parsedToDos = JSON.parse(loadedtoDos);
+        parsedToDos.forEach(function (toDo){
+            paintToDo(toDo.text);
+        });
+    } 
+}
+
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
