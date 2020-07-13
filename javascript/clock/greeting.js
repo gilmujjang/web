6339320ab8bc73fd1f0129b4f4b3b0e4e6c8ac1f
gilmujjang <alsanrlf@naver.com>
2020-07-13 14:28:16 +0900

    로컬스토리지에 값을 저장할수있음

--- javascript/clock/greeting.js
+++ javascript/clock/greeting.js
@@ -4,21 +4,39 @@ const USER_LS = "currentUser";
 const greeting = document.querySelector(".js-greetings");
 const SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
+ function handleSubmit(event){
+     event.preventDefault();
+     const currentValue = input.value;
+     paintGreeting(currentValue);
+     saveName(currentValue);
+ }
+
+function askForName(){
+    greeting.classList.remove(SHOWING_CN);
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-        paintGreeting("Unknown");
+        askForName();
     } 
     else{
         paintGreeting(currentUser);
     }
 }
+
+
 
 
 function init(){
