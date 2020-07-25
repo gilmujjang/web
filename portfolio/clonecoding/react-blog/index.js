e74babf44174a3f54e72917f8f7b92750add0e35
gilmu <alsanrlf@naver.com>
2020-07-25 20:03:01 +0900

    nodejs 와 react 를 사용한 블로그 클론코딩시작

---
+++ portfolio/clonecoding/react-blog/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://gilmu:root@react-blog.kd6dr.mongodb.net/<react-blog>?retryWrites=true&w=majority',
+ {useNewUrlParser: true}).then(() => console.log('DB connected')).catch(err => console.error(err));
+
+
+
+app.get('/', (req,res)=>{
+  res.send('hellow world')
+});
+
+
+
+
+app.listen(5000);
