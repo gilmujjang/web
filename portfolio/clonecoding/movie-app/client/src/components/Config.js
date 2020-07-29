ecaba4088f266405a5bff0af313609ecd5e0f852
gilmu <alsanrlf@naver.com>
2020-07-29 22:12:44 +0900

    랜딩페이지에 사진 불러옴

--- portfolio/clonecoding/movie-app/client/src/components/Config.js
+++ portfolio/clonecoding/movie-app/client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+export const IMAGE_BASW_URL = 'http://image.tmdb.org/t/p/'
+export const API_KEY = 'a4e90d748920f9cb18717a81e8986eb4'
