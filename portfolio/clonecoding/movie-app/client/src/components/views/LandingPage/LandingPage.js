224c94c6e926812605cb910733d54d57d3374e21
gilmu <alsanrlf@naver.com>
2020-07-30 14:13:58 +0900

    gridcards 만듬

--- portfolio/clonecoding/movie-app/client/src/components/views/LandingPage/LandingPage.js
+++ portfolio/clonecoding/movie-app/client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASW_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards'
+import { Row } from 'antd'
 
 function LandingPage() {
 
@@ -15,7 +17,7 @@ function LandingPage() {
       .then(response => response.json())
         .then(response => {
 
-          setMovies([response.results])
+          setMovies([...response.results])
           setMainMovieImage(response.results[0])
         })
   }, [])
@@ -38,6 +40,27 @@ function LandingPage() {
             <div style={{width: '85%', margin: '1rem auto'}}>
               <h2>Movies by latest</h2>
               <hr/>
+
+              {/* Movie Grid Cards */}
+              <Row gutter={[16, 16]}>
+
+                { Movies && Movies.map((movie, index) => (
+                  <React.Fragment key={index}>
+                    <GridCards
+                      image = {movie.poster_path ?
+                        `${IMAGE_BASW_URL}w500${movie.poster_path}` : null}
+                      movieId = {movie.id}
+                      movieName = {movie.original_title}
+                    />
+                  </React.Fragment>
+                ))}
+
+
+              </Row>
+
+
+
+
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center'}}>
