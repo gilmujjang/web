dd3699db34250cc853c5dfb368cd254cd862b839
gilmu <alsanrlf@naver.com>
2020-08-07 20:19:34 +0900

    Updates

---
+++ precache-manifest.05cd319c5d7956c6db2430deba8fe6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4704e439837ab11054ad9666f643063",
+    "url": "/web/portfolio/clonecoding/movie-app_nomadcoder/index.html"
+  },
+  {
+    "revision": "19c0612d0accef2de367",
+    "url": "/web/portfolio/clonecoding/movie-app_nomadcoder/static/css/main.4063452b.chunk.css"
+  },
+  {
+    "revision": "ba52b45166b2e6f56881",
+    "url": "/web/portfolio/clonecoding/movie-app_nomadcoder/static/js/2.cf993399.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web/portfolio/clonecoding/movie-app_nomadcoder/static/js/2.cf993399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c0612d0accef2de367",
+    "url": "/web/portfolio/clonecoding/movie-app_nomadcoder/static/js/main.f41b0a03.chunk.js"
+  },
+  {
+    "revision": "e838e08697ca00479956",
+    "url": "/web/portfolio/clonecoding/movie-app_nomadcoder/static/js/runtime-main.7da8a840.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/web/portfolio/clonecoding/movie-app_nomadcoder/precache-manifest.05cd319c5d7956c6db2430deba8fe6ab.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web/portfolio/clonecoding/movie-app_nomadcoder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
