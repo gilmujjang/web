0b2a9e19975be4f2474910125d2532dbf8fd387c
gilmujjang <alsanrlf@naver.com>
2020-09-01 17:55:37 +0900

    creative-landing-page

---
+++ small/creative-landing-page/main.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+  document.querySelector('.pepsi').src = anything;
+}
+function changeBgColor(color){
+  let sec = document.querySelector('.sec');
+  sec.style.background = color;
+}
