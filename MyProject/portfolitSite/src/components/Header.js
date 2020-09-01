87b6f1f6f086669d8940b8255d0c0f61684f11ee
gilmujjang <alsanrlf@naver.com>
2020-09-01 19:20:08 +0900

    어항 가운데로 맞춤

--- MyProject/portfolitSite/src/components/Header.js
+++ MyProject/portfolitSite/src/components/Header.js
@@ -5,7 +5,8 @@ class Header extends Component{
   render(){
     return(
       <div className="header">
-        <h2>포트폴리오</h2>
+        <h2>길무짱의 포트폴리오</h2>
+        <p>작동을 안하면 새로고침후 다시 해보세요</p>
       </div>
     )
   }
