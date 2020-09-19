45f13f3f3abd31f6f138186956fa1f7628e85cca
gilmujjang <alsanrlf@naver.com>
2020-09-19 22:56:46 +0900

    포폴사이트에서 클릭안되는 버그 수정

--- small/click-hover-event/main.js
+++ small/click-hover-event/main.js
@@ -1,7 +1,11 @@
-let cursor = document.getElementById('cursor');
 function cursorFuntion(e){
+  let cursor = document.getElementById('cursor');
   let x = e.clientX;
   let y = e.clientY;
   cursor.style.left = x + 'px';
   cursor.style.top = y + 'px';
+}
+
+function cursorOut(){
+  let cursor = 0;
 }
